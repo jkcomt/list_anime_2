2fa7e9f27b3a8f7e383e785808787c0fe0a12091
jkcomt <jkcomt@gmail.com>
2019-08-26 21:09:12 -0500

    added default image

--- lib/widgets/list_item.dart
+++ lib/widgets/list_item.dart
@@ -12,11 +12,11 @@ class ListItemAnime extends StatelessWidget {
   Widget build(BuildContext context) {
       return ListTile(
         onTap: (){},
-        leading: CircleAvatar(
-          backgroundImage: anime.attributes.coverImage.tiny != null ? NetworkImage(anime.attributes.coverImage.tiny) : null,        ),
+        leading: CircleAvatar(          
+          backgroundImage: anime.attributes.coverImage.tiny != null ? NetworkImage(anime.attributes.coverImage.tiny) : AssetImage('assets/img/placeholder.png')        ),
         title: Text(anime.attributes.canonicalTitle),
         subtitle: Text("Episodes: "+anime.attributes.episodeLength.toString()),
-        trailing: Icon(Icons.keyboard_arrow_right)
+        //trailing: Icon(Icons.keyboard_arrow_right)
       );
     }
 }
